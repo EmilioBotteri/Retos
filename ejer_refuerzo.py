8477e421860b60792b48c12d9952a4f91650b16d
Emilio Botteri <emiclau2001@hotmail.com>
2025-06-27 18:35:04 +0200

    Agrego ejercicios de refuerzo

---
+++ ejer_refuerzo.py
@@ -0,0 +1,32 @@
+
+# Ejercicio 1 
+# Escribir un programa que pregunte por consola por los  productos  de  una  cesta  de  la  compra,  
+# separados  por  comas,  y  muestre  por  pantalla  cada  uno  de  los  productos  en  una  línea  distinta. 
+
+
+
+# Ejercicio 2 
+# Escribir un programa que pregunte el nombre del usuario en la  consola  y  después  de  que  el  usuario  lo  
+# introduzca  muestre  por  pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en 
+# mayúsculas  y <n> es el número de letras que tienen el nombre. 
+
+
+# Ejercicio 3 
+# Escribir un programa que pida al usuario dos números enteros y  muestre por pantalla la <n> entre <m> da un 
+# cociente <c>  y un resto <r> donde <n> y <m> son los números introducidos  por el usuario, y <c> y <r> son el 
+# cociente y el resto de la división  entera respectivamente. 
+
+
+# Ejercicio 4 
+# Imagina  que  acabas  de  abrir  una  nueva  cuenta  de  ahorros  que  te ofrece el 4% de interés al año. 
+# Estos ahorros debido a  intereses, que no se cobran hasta finales de año, se te añaden al  balance final de 
+# tu cuenta de ahorros. Escribir un programa que  comience leyendo la cantidad de dinero depositada en la cuenta 
+# de  ahorros,  introducida  por  el  usuario.  Después  el  programa  debe calcular y mostrar por pantalla la 
+# cantidad de ahorros tras  el primer, segundo y tercer años. Redondear cada cantidad a dos decimales. 
+ 
+
+# Ejercicio 5 
+# Una panadería vende barras de pan a 3.49€ cada una. El pan que  no  es  el  día  tiene  un  descuento  del  60%. 
+# Escribir  un  programa  que comience leyendo el número de barras vendidas que no son  del día. Después el 
+# programa debe mostrar el precio habitual de  una barra de pan, el descuento que se le hace por no ser fresca 
+# y el coste final total. 
